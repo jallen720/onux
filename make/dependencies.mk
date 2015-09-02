269a36cc03f0eea3fde0d78d69e60167a21355a1
Joel Allen <joelallen720@gmail.com>
2015-09-02 01:24:38 -0700

    Update dependencies.

--- make/dependencies.mk
+++ make/dependencies.mk
@@ -2,13 +2,13 @@
 
 # Include directories
 APP_INCS    = -I$(INCLUDE_DIR)
-MAGICK_INCS = `pkg-config Magick++ --cflags`
+MAGICK_INCS = `pkg-config --cflags Magick++`
 INCS        = $(APP_INCS) $(MAGICK_INCS)
 
 # Libraries
 APP_LIBS    =
 GTEST_LIBS  = -lgtest -lpthread
-GLEW_LIBS   = -lGLEW
+GLEW_LIBS   = `pkg-config --libs glew`
 GLFW3_LIBS  = `pkg-config --static --libs glfw3`
 MAGICK_LIBS = `pkg-config --static --libs Magick++`
 ASSIMP_LIBS = `pkg-config --libs assimp`
