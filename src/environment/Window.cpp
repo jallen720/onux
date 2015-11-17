e7302894626f68bb1849b0c0a679fa8405981407
Joel Allen <joelallen720@gmail.com>
2015-11-17 11:05:05 -0800

    Clean Window.cpp.

--- src/environment/Window.cpp
+++ src/environment/Window.cpp
@@ -58,18 +58,15 @@ Input& Window::getInput() {
 
 // Implementation
 
-static bool isValidDimension(const unsigned int dimension) {
+static void validateDimension(const unsigned int dimension, const string& dimensionName) {
   static const auto MIN_DIMENSION_SIZE = 1u;
-  return dimension >= MIN_DIMENSION_SIZE;
-}
 
-static void validateDimesion(const unsigned int dimension, const string dimensionName) {
-  if (!isValidDimension(dimension)) {
+  if (dimension < MIN_DIMENSION_SIZE) {
     throw InvalidArg(
       dimensionName,
       "Window",
       to_string(dimension),
-      "> 0"
+      ">= " + to_string(MIN_DIMENSION_SIZE)
     );
   }
 }
@@ -81,13 +78,9 @@ static bool isEmpty(const char* name) {
 static void validateName(const char* name) {
   if (name == nullptr) {
     throw NullArg("name", "Window");
-  } else if (isEmpty(name)) {
-    throw InvalidArg(
-      "name",
-      "Window",
-      "\"\"",
-      "non-empty"
-    );
+  }
+  else if (isEmpty(name)) {
+    throw InvalidArg("name", "Window", "\"\"", "non-empty");
   }
 }
 
@@ -96,8 +89,8 @@ static GLFWwindow* loadWindow(
   const unsigned int height,
   const char*        name
 ) {
-  validateDimesion(width, "width");
-  validateDimesion(height, "height");
+  validateDimension(width, "width");
+  validateDimension(height, "height");
   validateName(name);
   return glfwCreateWindow(width, height, name, nullptr, nullptr);
 }
