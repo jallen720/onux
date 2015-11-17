fd085f05e675fa84e513814490c594cebe8700dc
Joel Allen <joelallen720@gmail.com>
2015-11-17 11:06:03 -0800

    Clean ShaderObject.cpp.

--- src/gl/ShaderObject.cpp
+++ src/gl/ShaderObject.cpp
@@ -57,14 +57,11 @@ static void validateSameType(ShaderObject::Sources sources, const GLenum type) {
   }
 }
 
-static const GLenum loadType(ShaderObject::Sources sources, const GLenum type) {
-  validateSameType(sources, type);
-  return type;
-}
-
 static const GLuint loadShaderObject(ShaderObject::Sources sources) {
   validateSourceCount(sources.size());
-  return glCreateShader(loadType(sources, sources[0]->getType()));
+  const GLenum type = sources[0]->getType();
+  validateSameType(sources, type);
+  return glCreateShader(type);
 }
 
 ShaderObject::ShaderObject(Sources sources)
