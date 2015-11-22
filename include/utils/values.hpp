8166d7bcde0ae2b21fea51d230ceb39e92c7b45a
Joel Allen <joelallen720@gmail.com>
2015-11-22 10:33:34 -0800

    Add values() util function. Clean keys.hpp.

--- include/utils/values.hpp
+++ include/utils/values.hpp
@@ -8,9 +8,9 @@
 namespace onux {
 
 template<typename Key, typename Value>
-const std::vector<Key> keys(const std::map<const Key, const Value>& map) {
-  return onux::map<Key>(map, [](const auto& pair) {
-    return pair.first;
+const std::vector<Value> values(const std::map<const Key, const Value>& map) {
+  return onux::map<Value>(map, [](const auto& pair) {
+    return pair.second;
   });
 }
 
