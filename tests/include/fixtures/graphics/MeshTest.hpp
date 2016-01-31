2763b59205519a7a9a520849ed0fb75f71ffab60
Joel Allen <joelallen720@gmail.com>
2016-01-31 15:08:41 -0500

    Clean MeshTest.hpp.

--- tests/include/fixtures/graphics/MeshTest.hpp
+++ tests/include/fixtures/graphics/MeshTest.hpp
@@ -9,15 +9,18 @@
 
 struct MeshTest : testing::Test {
 private:
+    const unsigned int POST_PROCESSING_FLAGS =
+        aiProcess_GenSmoothNormals      |
+        aiProcess_CalcTangentSpace      |
+        aiProcess_Triangulate           |
+        aiProcess_JoinIdenticalVertices |
+        aiProcess_SortByPType;
+
     Assimp::Importer importer;
 
 protected:
     const aiMesh* cubeMesh = importer.ReadFile(
         testScenePath("cube.obj"),
-        aiProcess_GenSmoothNormals      |
-        aiProcess_CalcTangentSpace      |
-        aiProcess_Triangulate           |
-        aiProcess_JoinIdenticalVertices |
-        aiProcess_SortByPType
+        POST_PROCESSING_FLAGS
     )->mMeshes[0];
 };
