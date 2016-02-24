4a37eb2f709bd5414dd46cf1324ae74a001367c8
Joel Allen <joelallen720@gmail.com>
2016-02-24 13:27:31 -0800

    Add filesInDirectory() test for passing invalid directory.

--- engine/tests/src/resources/utils/filesInDirectory.cpp
+++ engine/tests/src/resources/utils/filesInDirectory.cpp
@@ -1,12 +1,18 @@
 #include <gtest/gtest.h>
+#include <boost/filesystem.hpp>
 
 #include "resources/utils/filesInDirectory.hpp"
 #include "exceptions/argErrors/EmptyStringArg.hpp"
 
 using std::string;
+using boost::filesystem::filesystem_error;
 using onux::filesInDirectory;
 using onux::EmptyStringArg;
 
 TEST(filesInDirectoryTest, emptyDirectory) {
     EXPECT_THROW(filesInDirectory("", {}), EmptyStringArg);
+}
+
+TEST(filesInDirectoryTest, invalidDirectory) {
+    EXPECT_THROW(filesInDirectory("invalid/directory", {}), filesystem_error);
 }
