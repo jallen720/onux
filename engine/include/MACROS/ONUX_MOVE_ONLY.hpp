a98517e38ba643303d3d63199a11b4db0b632e47
Joel Allen <joelallen720@gmail.com>
2016-02-28 15:38:10 -0800

    Clean move-only macros.

--- engine/include/MACROS/ONUX_MOVE_ONLY.hpp
+++ engine/include/MACROS/ONUX_MOVE_ONLY.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#define ONUX_MOVE_ONLY(OBJECT) \
-    OBJECT(const OBJECT& original) = delete; \
-    OBJECT(OBJECT&& original) = default; \
-    ~OBJECT() = default; \
-    OBJECT& operator =(const OBJECT& original) = delete; \
-    OBJECT& operator =(OBJECT&& original) = default;
+#define ONUX_MOVE_ONLY(OBJECT)\
+OBJECT(const OBJECT& original) = delete;\
+OBJECT(OBJECT&& original) = default;\
+~OBJECT() = default;\
+OBJECT& operator =(const OBJECT& original) = delete;\
+OBJECT& operator =(OBJECT&& original) = default;
--- engine/include/MACROS/ONUX_MOVE_ONLY_DEFS.hpp
+++ engine/include/MACROS/ONUX_MOVE_ONLY_DEFS.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#define ONUX_MOVE_ONLY_DEFS(OBJECT) \
-    OBJECT(const OBJECT& original) = delete; \
-    OBJECT(OBJECT&& original); \
-    ~OBJECT(); \
-    OBJECT& operator =(const OBJECT& original) = delete; \
-    OBJECT& operator =(OBJECT&& original);
+#define ONUX_MOVE_ONLY_DEFS(OBJECT)\
+OBJECT(const OBJECT& original) = delete;\
+OBJECT(OBJECT&& original);\
+~OBJECT();\
+OBJECT& operator =(const OBJECT& original) = delete;\
+OBJECT& operator =(OBJECT&& original);
