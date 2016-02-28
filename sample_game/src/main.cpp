ebac92d18b9493a3c656dd50436afc800657f295
Joel Allen <joelallen720@gmail.com>
2016-02-28 11:52:33 -0800

    Update .gitignore with debugging and blender directories. Add simple debugging to main.

--- sample_game/src/main.cpp
+++ sample_game/src/main.cpp
@@ -1,6 +1,10 @@
 #include "runEngine.hpp"
+// #include "debugging/printAssimpScene.hpp"
+
+// using onux::printAssimpScene;
 
 int main(int argc, char* argv[]) {
     runEngine();
+    // printAssimpScene("resources/modelScenes/box.fbx");
     return 0;
 }
