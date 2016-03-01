c7f365b960c620cde9f25d44811b0435e9d19006
Joel Allen <joelallen720@gmail.com>
2016-02-29 19:25:51 -0800

    Fix getExtension() passing string instead of char to string::find_last_of().

--- engine/src/utils/getExtension.cpp
+++ engine/src/utils/getExtension.cpp
@@ -5,7 +5,7 @@ using std::string;
 namespace onux {
 
 const string getExtension(const string& path) {
-    const size_t dotPosition = path.find_last_of(".");
+    const size_t dotPosition = path.find_last_of('.');
 
     return dotPosition != string::npos
            ? path.substr(dotPosition + 1)
